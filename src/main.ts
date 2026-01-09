46e8075e3944743a5d04ce9818c15ebb54469600
Rakhmiddin.Asiydinov <rahmiddinasiydinov@gmail.com>
2026-01-09 10:59:06 +0500

    fix: add deployed frontend url to CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://ox-sys-front.vercel.app',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
